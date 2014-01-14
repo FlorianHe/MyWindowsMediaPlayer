3711f69d9218af8450fc93ce8e702f2569cc04e3
Starti <bourqu.g@gmail.com>
2014-01-14 11:33:57 +0100

    derniere version
    la derniere version

--- lecteur/MainWindow.xaml.cs
+++ lecteur/MainWindow.xaml.cs
@@ -18,33 +18,58 @@ using System.Diagnostics;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Windows.Controls.Primitives;
+using System.ComponentModel;
+
 
 namespace lecteur
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         DispatcherTimer timer;
         OpenFileDialog ofd = new OpenFileDialog();
+        ObservableCollection<Playlist> saveplayxml = new ObservableCollection<Playlist>();
+        ObservableCollection<Playlist> nameplay = new ObservableCollection<Playlist>();
+        ObservableCollection<Song> gsong = new ObservableCollection<Song>();
+        ObservableCollection<Song> savexml = new ObservableCollection<Song>();
+        double savevolume;
+        bool fullscreen = false;
+        bool isDragging = false;
+        double hauteur;
+        double largeur;
 
         public MainWindow()
         {
             InitializeComponent();
-           
+            this.DataContext = this;
+
+            hauteur = mediaElement1.Height;
+            largeur = mediaElement1.Width;
+            string rep = Environment.CurrentDirectory;
+            rep += "\\..\\..\\Icones\\";
+            this.Background = new ImageBrush(new BitmapImage(new Uri(rep + "Fond_basic2.jpg")));
+
+            //Affichage des playlists
+            refreshlist();
+
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Interval = TimeSpan.FromMilliseconds(200);
             timer.Tick += new EventHandler(timer_Tick);
 
         }
 
         void timer_Tick(object sender, EventArgs e)
         {
-            slider_seek.Value = mediaElement1.Position.TotalSeconds;
-
+            if (!isDragging)
+                slider_seek.Value = mediaElement1.Position.TotalSeconds;
         }
 
+        //Bouton play
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (mediaElement1.Source != null)
@@ -52,40 +77,65 @@ namespace lecteur
                 mediaElement1.Visibility = System.Windows.Visibility.Visible;
                 mediaElement1.Play();
             }
+            else
+            {
+                ofd.Filter = "MP4 (*.mp4)|*.mp4|MP3's (*.mp3)|*.mp3|AVI (*.avi)|*.avi|All files (*.*)|*.*";
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    mediaElement1.Source = new Uri(ofd.FileName);
+                    mediaElement1.LoadedBehavior = MediaState.Manual;
+                    mediaElement1.UnloadedBehavior = MediaState.Manual;
+                    mediaElement1.Play();
+                }
+            }
         }
 
+        //Bouton Pause
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             if (mediaElement1.Source != null)
-            mediaElement1.Pause();
+                mediaElement1.Pause();
         }
 
+        //Bouton Stop
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             if (mediaElement1.Source != null)
-            mediaElement1.Stop();
+                mediaElement1.Stop();
         }
 
+        //Volume
         private void slider_vol_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             mediaElement1.Volume = (double)slider_vol.Value;
         }
 
-        private void slider_seek_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void muet_Click(object sender, RoutedEventArgs e)
         {
-            mediaElement1.Position = TimeSpan.FromSeconds(slider_seek.Value);
+            muet.Visibility = System.Windows.Visibility.Hidden;
+            resetvol.Visibility = System.Windows.Visibility.Visible;
+            mediaElement1.Volume = savevolume;
+            slider_vol.Value = savevolume;
         }
 
-        private void Window_Drop(object sender, System.Windows.DragEventArgs e)
+        private void resetvol_Click(object sender, RoutedEventArgs e)
         {
-            string filename = (string)((System.Windows.DataObject)e.Data).GetFileDropList()[0];
-            mediaElement1.Source = new Uri(filename);
+            resetvol.Visibility = System.Windows.Visibility.Hidden;
+            muet.Visibility = System.Windows.Visibility.Visible;
+            savevolume = mediaElement1.Volume;
+            mediaElement1.Volume = 0;
+            slider_vol.Value = 0;
+        }
 
-            mediaElement1.LoadedBehavior = MediaState.Manual;
-            mediaElement1.UnloadedBehavior = MediaState.Manual;
-            mediaElement1.Volume = (double)slider_vol.Value;
-            mediaElement1.Play();
+        private void seekBar_DragStarted(Object sender, DragStartedEventArgs e)
+        {
+            isDragging = true;
+        }
 
+        private void seekBar_DragCompleted(Object sender, DragCompletedEventArgs e)
+        {
+            isDragging = false;
+            mediaElement1.Position = TimeSpan.FromSeconds(slider_seek.Value);
         }
 
         private void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
@@ -94,11 +144,13 @@ namespace lecteur
             {
                 TimeSpan ts = mediaElement1.NaturalDuration.TimeSpan;
                 slider_seek.Maximum = ts.TotalSeconds;
-                timer.Start();
+                slider_seek.SmallChange = 1;
+                slider_seek.LargeChange = Math.Min(10, ts.Seconds / 10);
             }
-
+            timer.Start();
         }
 
+        //Ouvrir les fichiers par le répertoire
         private void Ouvrir(object sender, RoutedEventArgs e)
         {
             ofd.Filter = "MP4 (*.mp4)|*.mp4|MP3's (*.mp3)|*.mp3|AVI (*.avi)|*.avi|All files (*.*)|*.*";
@@ -111,6 +163,24 @@ namespace lecteur
             }
         }
 
+        //FullScreen
+        private void mediaElement1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && fullscreen == false)
+            {
+                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Maximized;
+                fullscreen = true;
+            }
+            else if (e.ClickCount == 2 && fullscreen == true)
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.WindowState = WindowState.Normal;
+                fullscreen = false;
+            }
+        }
+
+        //Fermeture du programme
         private void Quitter(object sender, RoutedEventArgs e)
         {
             Close();
@@ -118,49 +188,313 @@ namespace lecteur
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-
         }
 
+        //Affichage des boutons pour la création des playlist
+        private void Create_playlist(object sender, RoutedEventArgs e)
+        {
+            crtext.Visibility = System.Windows.Visibility.Visible;
+            Creer.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        //Affichage des membres du projets
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
             Window1 test = new Window1();
             test.Show();
         }
 
-
-        private void test_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        //Mise a jour des playlists
+        public void refreshlist()
         {
+            string[] file = Directory.GetFiles("../../", "*.xml");
+            Playlist woot;
+            foreach (string dir in file)
+            {
+                string value = System.IO.Path.GetFileNameWithoutExtension(dir);
+                woot = new Playlist();
+                woot.setname(value);
+                nameplay.Add(woot);
+            }
+        }
+
+        //Biding mylist
+        public ObservableCollection<Playlist> Playlists
+        {
+            get { return nameplay; }
+        }
+
+        //Biding songlist
+        public ObservableCollection<Song> Songs
+        {
+            get { return gsong; }
+        }
+
+        //Génération de la playlist, affichage son
+        private void mylist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Réduction du mediaelement
             mediaElement1.Width = 200;
             mediaElement1.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
             mediaElement1.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+
+            //Récupération de la playlist
+            int index = Convert.ToInt32(mylist.SelectedIndex.ToString());
+            Playlist value = new Playlist();
+            if (index >= 0 && index < mylist.Items.Count)
+            {
+                value = (Playlist)mylist.Items[index];
+                string file = value.name;
+                file = "../../" + file + ".xml";
+
+                //Affichage du titre de la playlist en cours et du boutonsave
+                titleplay.Content = value.name;
+                titleplay.Visibility = System.Windows.Visibility.Visible;
+                saveplay.Visibility = System.Windows.Visibility.Visible;
+
+                ObservableCollection<Playlist> myplay = new ObservableCollection<Playlist>();
+                XmlSerializer ps = new XmlSerializer(typeof(ObservableCollection<Playlist>));
+                using (StreamReader rd = new StreamReader(file))
+                {
+                    myplay = ps.Deserialize(rd) as ObservableCollection<Playlist>;
+                }
+                gsong.Clear();
+
+                //Affichage des sons de la playlist
+                int i = 0;
+                Song displaysong;
+                while (i < myplay[0].LSongs.Count)
+                {
+                    displaysong = new Song();
+                    displaysong.name = myplay[0].LSongs[i].name;
+                    displaysong.artist = myplay[0].LSongs[i].artist;
+                    displaysong.genre = myplay[0].LSongs[i].genre;
+                    displaysong.chemin = myplay[0].LSongs[i].chemin;
+                    gsong.Add(displaysong);
+                    i++;
+                }
+            }
+            songlist.Visibility = System.Windows.Visibility.Visible;
         }
 
-        private void createpl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        //Creation de la playlist
+        private void Creer_Click(object sender, RoutedEventArgs e)
         {
-            Song w = new Song("chut", "marcha", "rap", "test");
-            Song x = new Song("chutt", "marchaa", "raap", "teest");
-            Console.WriteLine(w.name);
-            Playlist p = new Playlist();
-            p.setname("wlala");
-            p.addsong(w);
-            p.addsong(x);
-
-            System.IO.FileStream fs = System.IO.File.Create("../../test.xml");
+            Console.WriteLine("test");
+            string value = crtext.Text;
+            value = "../../" + value + ".xml";
+            Console.WriteLine(value);
+            System.IO.FileStream fs = System.IO.File.Create(value);
             fs.Close();
 
-            XmlSerializer xs = new XmlSerializer(typeof(Playlist));
-            using (StreamWriter wr = new StreamWriter("../../test.xml"))
+            ObservableCollection<Playlist> otmp = new ObservableCollection<Playlist>();
+            Playlist tmp = new Playlist();
+            tmp.name = crtext.Text;
+            otmp.Add(tmp);
+
+            XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<Playlist>));
+            using (StreamWriter wr = new StreamWriter(value))
             {
-                xs.Serialize(wr, p);
+                xs.Serialize(wr, otmp);
             }
-            
-            Playlist g = new Playlist();
-            XmlSerializer ps = new XmlSerializer(typeof(Playlist));
-            using (StreamReader rd = new StreamReader("../../test.xml"))
+
+            crtext.Visibility = System.Windows.Visibility.Hidden;
+            Creer.Visibility = System.Windows.Visibility.Hidden;
+            nameplay.Clear();
+            refreshlist();
+            OnPropertyChanged("Playlists");
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        //Lecture des musiques
+        private void lectsong(int index, int ajout)
+        {
+            Song readsong;
+            Console.WriteLine(index);
+
+            readsong = new Song();
+            if ((index + ajout) >= 0 && (index + ajout) < songlist.Items.Count)
             {
-                g = ps.Deserialize(rd) as Playlist;
-                Console.WriteLine(g.name);
+                readsong = (Song)songlist.Items[index + ajout];
+                string filename = readsong.chemin;
+
+                Boolean file = System.IO.File.Exists(filename);
+                if (file == true)
+                {
+                    mediaElement1.Source = new Uri(filename);
+                    mediaElement1.LoadedBehavior = MediaState.Manual;
+                    mediaElement1.UnloadedBehavior = MediaState.Manual;
+                    mediaElement1.Volume = (double)slider_vol.Value;
+                    mediaElement1.Play();
+                }
+                else
+                {
+                    ErreurFile error = new ErreurFile();
+                    error.Show();
+                }
             }
         }
+
+        private void songlist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            int index = Convert.ToInt32(songlist.SelectedIndex.ToString());
+            lectsong(index, 0);
+            
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Convert.ToInt32(songlist.SelectedIndex.ToString());
+            lectsong(index, -1);
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Convert.ToInt32(songlist.SelectedIndex.ToString());
+            lectsong(index, 1);
+        }
+
+
+        //Drop des fichiers dans la playlist
+        private void songlist_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            string chem = (string)((System.Windows.DataObject)e.Data).GetFileDropList()[0];
+            string filename = System.IO.Path.GetFileNameWithoutExtension(chem);
+            Song nesong = new Song();
+            nesong.name = filename;
+            nesong.chemin = chem;
+            gsong.Add(nesong);
+        }
+
+        //Sauvegarde de la playlist
+        private void saveplay_Click(object sender, RoutedEventArgs e)
+        {
+            Song tmp;
+            Song savesong;
+            Playlist saveplaylist = new Playlist();
+            saveplaylist.name = titleplay.Content.ToString();
+            Console.WriteLine(saveplaylist.name);
+            int i = 0;
+            while (i < songlist.Items.Count)
+            {
+                tmp = new Song();
+                savesong = new Song();
+                tmp = (Song)songlist.Items[i];
+                savesong.name = tmp.name;
+                savesong.chemin = tmp.chemin;
+                saveplaylist.LSongs.Add(savesong);
+                i++;
+            }
+            saveplayxml.Add(saveplaylist);
+
+            XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<Playlist>));
+            using (StreamWriter wr = new StreamWriter("../../"+saveplaylist.name+".xml"))
+            {
+                xs.Serialize(wr, saveplayxml);
+            }
+
+        }
+
+        private void mediaElement1_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            songlist.Visibility = System.Windows.Visibility.Hidden;
+            titleplay.Visibility = System.Windows.Visibility.Hidden;
+            saveplay.Visibility = System.Windows.Visibility.Hidden;
+
+            mediaElement1.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+            mediaElement1.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            mediaElement1.Height = hauteur;
+            mediaElement1.Width = largeur;
+        }
+
+        //Changement des thèmes
+        private string initheme()
+        {
+            Play.Content = null;
+            Pause.Content = null;
+            Stop.Content = null;
+            muet.Content = null;
+            resetvol.Content = null;
+            Creer.Content = null;
+            saveplay.Content = null;
+            screenfull.Content = null;
+            string rep = Environment.CurrentDirectory;
+            rep += "\\..\\..\\Icones\\";
+            return rep;
+        }
+
+        private void choosetheme(string rep, string nb)
+        {
+            Play.Background = new ImageBrush(new BitmapImage(new Uri(rep + "play" + nb + ".png")));
+            Pause.Background = new ImageBrush(new BitmapImage(new Uri(rep + "pause" + nb + ".png")));
+            Stop.Background = new ImageBrush(new BitmapImage(new Uri(rep + "stop" + nb + ".png")));
+            muet.Background = new ImageBrush(new BitmapImage(new Uri(rep + "son_off" + nb + ".png")));
+            resetvol.Background = new ImageBrush(new BitmapImage(new Uri(rep + "son_on" + nb + ".png")));
+            Creer.Background = new ImageBrush(new BitmapImage(new Uri(rep + "creer" + nb + ".png")));
+            saveplay.Background = new ImageBrush(new BitmapImage(new Uri(rep + "save" + nb + ".png")));
+            screenfull.Background = new ImageBrush(new BitmapImage(new Uri(rep + "fullscreen" + nb + ".png")));
+        }
+        private void ColorDefault(object sender, RoutedEventArgs e)
+        {
+            string rep = initheme();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(rep+"Fond_basic2.jpg")));
+            choosetheme(rep, "5");
+        }
+
+        private void ColorOcean(object sender, RoutedEventArgs e)
+        {
+            string rep = initheme();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(rep + "Fond_ocean.jpg")));
+            choosetheme(rep, "4");
+        }
+
+        private void ColorPigeon(object sender, RoutedEventArgs e)
+        {
+            string rep = initheme();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(rep + "Fond_animaux.jpg")));
+            choosetheme(rep, "");
+        }
+
+        private void ColorCiel(object sender, RoutedEventArgs e)
+        {
+            string rep = initheme();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(rep + "Fond_basic3.png")));
+            choosetheme(rep, "3");
+        }
+
+        private void ColorNuit(object sender, RoutedEventArgs e)
+        {
+            string rep = initheme();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(rep + "Fond_basic.jpg")));
+            choosetheme(rep, "2");
+        }
+
+        private void screenfull_Click(object sender, RoutedEventArgs e)
+        {
+            if (fullscreen == false)
+            {
+                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Maximized;
+                fullscreen = true;
+            }
+            else if (fullscreen == true)
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.WindowState = WindowState.Normal;
+                fullscreen = false;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
---
+++ lecteur/Playlist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Collections.ObjectModel;
+
+namespace lecteur
+{
+    public class Playlist
+    {
+        public string name;
+        public ObservableCollection<Song> LSongs = new ObservableCollection<Song>();
+
+        public string getname
+        {
+            get { return name; }
+        }
+        public void setname(string newname)
+        {
+            this.name = newname;
+        }
+
+        public void addsong(Song songs)
+        {
+            LSongs.Add(songs);
+        }
+
+    }
+}
